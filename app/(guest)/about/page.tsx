6f4b419567c9dfdb7219070893ba919462a6548a
Zach Abellar <zachbanned23@gmail.com>
2026-02-04 15:55:49 -0700

    guest

--- app/(guest)/about/page.tsx
+++ app/(guest)/about/page.tsx
@@ -3,8 +3,10 @@ import { Button } from "@/components/ui/button";
 export default function AboutPage() {
   return (
     <div>
+      <main className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1>About Me</h1>
       <Button>Button Here</Button>
+      </main>
     </div>
   )
 }
