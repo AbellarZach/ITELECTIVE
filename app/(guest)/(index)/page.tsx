b3a8ab794c9ff615acb2bb4d8cf8fd3b7c8cf395
Zach Abellar <zachbanned23@gmail.com>
2026-02-04 15:34:02 -0700

    first commit

--- app/(guest)/(index)/page.tsx
+++ app/(guest)/(index)/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Home Page</h1>
       <Button>Button Here</Button>
-    </div>
-  )
+    </main>
+  );
 }
