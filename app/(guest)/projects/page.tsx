b705b638fd52b118d452904aaf6621638130f8f2
Zach Abellar <zachbanned23@gmail.com>
2026-02-04 16:54:55 -0700

    projects

--- app/(guest)/projects/page.tsx
+++ app/(guest)/projects/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 
 export default function ProjectsPage() {
-  return (
-    <div>
-      <h1>Projects</h1>
-      <Button>Button Here</Button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Projects</h1>
+            <Button>Contact Me</Button>
+        </div>
+    )
 }
