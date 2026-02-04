144b9c513b10624b822e47c45311591861399572
Zach Abellar <zachbanned23@gmail.com>
2026-02-04 16:58:45 -0700

    admin manual

--- app/admin/(index)/page.tsx
+++ app/admin/(index)/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function AdminPage() {
   return (
-    <main>
-      <h1>Admin Page</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold mb-4">Admin Page</h1>
       <Button>Button Here</Button>
     </main>
-  )
+  );
 }
