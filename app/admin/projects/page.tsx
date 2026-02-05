ce7c8eab605aef0a9d7c6201d3cb9ec4b056574b
Zach Abellar <zachbanned23@gmail.com>
2026-02-04 17:01:37 -0700

    admin project manual

--- app/admin/projects/page.tsx
+++ app/admin/projects/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function AdminProjectsPage() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
       <h1>Admin Projects Management Page</h1>
       <Button>Button Here</Button>
     </main>
